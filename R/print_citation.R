36d366e2f9182a25e3cc61fc7bb8f6252c62d644
Josh Fjelstul <joshfjelstul@gmail.com>
2024-07-21 12:35:27 -0500

    Update citations

--- R/print_citation.R
+++ R/print_citation.R
@@ -18,43 +18,19 @@
 #' @export
 print_citation <- function(component) {
 
-  if (component == "cjeu_database_platform") {
-    database <- "the IUROPA CJEU Database Platform"
-  }
+  # make URL
+  url <- stringr::str_c(
+    get_api_address(),
+    "database/documentation/citation/",
+    component
+  )
 
-  if (component == "cjeu_text_corpus") {
-    database <- "the IUROPA CJEU Text Corpus"
-  }
+  # request citation
+  citation <- make_request(url, quietly = TRUE)
 
-  if (component == "issues_and_positions") {
-    database <- "the IUROPA CJEU Database: Issues and Positions Component"
-  }
+  # coerce to a string
+  citation <- as.character(citation$value)
 
-  cat("\n")
-  cat("If you use data from ", database, " in a paper, please cite the database:\n", sep = "")
-  cat("\n")
-
-  if (component == "cjeu_database_platform") {
-    cat("Brekke, Stein Arne, Joshua Fjelstul, Silje Synnøve Lyder Hermansen, and Daniel Naurin. 2023. \"The IUROPA CJEU Database Platform: Decisions and Decision-Makers\" (Release 1.0), in Lindholm, Johan, Daniel Naurin, Urska Sadl, Anna Wallerman Ghavanini, Stein Arne Brekke, Joshua Fjelstul, Silje Synnøve Lyder Hermansen, Olof Larsson, Andreas Moberg, Moa Näsström, Michal Ovádek, Tommaso Pavone, and Philipp Schroeder, The IUROPA CJEU Database, The IUROPA Project, https://iuropa.pol.gu.se/.\n", sep = "")
-  }
-
-  if (component == "cjeu_text_corpus") {
-    cat("Fjelstul, Joshua, Johan Lindholm, Daniel Naurin, and Michal Ovádek. 2023. \"The IUROPA CJEU Text Corpus\" (Release 1.0), in Lindholm, Johan, Daniel Naurin, Urska Sadl, Anna Wallerman Ghavanini, Stein Arne Brekke, Joshua Fjelstul, Silje Synnøve Lyder Hermansen, Olof Larsson, Andreas Moberg, Moa Näsström, Michal Ovádek, Tommaso Pavone, and Philipp Schroeder, The IUROPA CJEU Database, The IUROPA Project, https://iuropa.pol.gu.se/.\n", sep = "")
-  }
-
-  if (component == "issues_and_positions") {
-    cat("Larsson, Olof, Johan Lindholm, Daniel Naurin, Andreas Moberg, Philipp Schroeder, Anna Wallerman Ghavanini, Sebastian Björnberg, Aaron Coster, Moa Näsström, and Irene Otero. 2023. \"The IUROPA CJEU Database: Issues and Positions Component\" (Release 1.0), in Lindholm, Johan, Daniel Naurin, Urška Šadl, Anna Wallerman Ghavanini, Stein Arne Brekke, Joshua Fjelstul, Silje Synnøve Lyder Hermansen, Olof Larsson, Andreas Moberg, Moa Näsström, Michal Ovádek, Tommaso Pavone, and Philipp Schroeder, The IUROPA CJEU Database, The IUROPA Project, https://iuropa.pol.gu.se/.\n", sep = "")
-  }
-
-  if (component == "cjeu_database_platform") {
-    cat("\n")
-    cat("Please also cite the article that introduces the IUROPA CJEU Database Platform:\n")
-    cat("\n")
-    cat("Stein Arne Brekke, Joshua C. Fjelstul, Silje Synnøve Lyder Hermansen, and Daniel Naurin. 2023. \"The CJEU Database Platform: Decisions and Decision-Makers.\" The Journal of Law and Courts. Forthcoming.\n")
-  }
-
-  cat("\n")
-  cat("Please also cite the iuropa R package:\n")
-  cat("\n")
-  cat("Joshua C. Fjelstul (2023). iuropa: An R Interface to the IUROPA API. R package version ", as.character(packageVersion("iuropa")), ". https://github.com/jfjelstul/iuropa.\n", sep = "")
+  # print citation
+  cat(citation)
 }
